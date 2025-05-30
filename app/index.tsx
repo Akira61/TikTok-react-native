ed976d7bc9f367f67e585da8db65054d65992009
fahad al shehri <otaku.fahad11@gmail.com>
2025-05-30 11:00:23 +0300

    get prayer time from api

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
 import { Alert, View } from "react-native";
 import * as Location from "expo-location";
 import { useEffect, useState } from "react";
+import dayjs from "dayjs";
 
 export default function Index() {
   const [latitude, setLatitude] = useState<number>();
   const [longitude, setLongitude] = useState<number>();
-
+  const [timings, setTimings] = useState<any>({});
+  
   // get user's location
   useEffect(() => {
     const getLocation = async () => {
@@ -25,6 +27,21 @@ export default function Index() {
     }
     getLocation();
 
+    // When lat/lng is available, fetch prayer times
+  useEffect(() => {
+    if(latitude && longitude){
+      const today = dayjs().format("D-M-YYYY");
+      const getPrayerTime = async () => {
+        const URL = `https://api.aladhan.com/v1/timings/${today}?latitude=${latitude}&longitude=${longitude}`;
+        const request = await fetch(URL);
+        const response = await request.json();
+        const timings = response.data.timings;
+        setTimings(timings);
+      };
+      //
+      getPrayerTime();
+    }
+  }, [latitude, longitude]);
   }, []);
   
   return (
